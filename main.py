229a9f3e8b11f80881f319f308afcad5a7533d6d
PatriciaMurotani <patricia.murotani@gmail.com>
2023-02-27 21:30:39 -0300

    Atualiza classes

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
-import re
-from telefones_br import TelefonesBr
+import requests
 
-telefone = "551145671234"
-telefone_objeto = TelefonesBr(telefone)
-#padrao = "([0-9]{2,3})?([0-9]{2})([0-9]{4,5})([0-9]{4})"
-#reposta = re.findall(padrao, telefone)
-print(telefone_objeto)
+from datetime import datetime, timedelta
+from datas_br import DatasBr
+from acesso_cep import BuscaEndereco
+
+cep = "01405200"
+objeto_cep = BuscaEndereco(cep)
+
+#r = requests.get("https://viacep.com.br/ws/01001000/json/")
+#print(r.text)
+
+objeto_cep = BuscaEndereco(cep)
+
+bairro, cidade, uf = objeto_cep.acessa_via_cep()
+print(bairro, cidade, uf)
+
+
+
+'''
+a = objeto_cep.acessa_via_cep()
+
+print(type(a.text))
+print(a.text)
+
+print(type(a.json()))
+print(a.json())
+print(a.json()['bairro'])
+
+hoje = DatasBr()
+print(hoje.tempo_cadastro())
+
+cadastro = DatasBr()
+print(cadastro)
+
+hoje =  datetime.today()
+hoje_formatado = hoje.strftime("%d/%m/%Y  %H:%M")
+print(hoje)
+print(hoje_formatado)
+'''
